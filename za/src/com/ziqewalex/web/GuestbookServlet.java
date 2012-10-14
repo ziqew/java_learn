a446140151ec8599de04d5b23fca52d52de6d8cc
ziqew <gongwenwei@gmail.com>
2012-10-14 22:22:33 +0800

    gae and gwt app

---
+++ za/src/com/ziqewalex/web/GuestbookServlet.java
@@ -0,0 +1,15 @@
+package com.ziqewalex.web;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class GuestbookServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		resp.setContentType("text/plain");
+		resp.getWriter().println("Hello, world");
+	}
+}
