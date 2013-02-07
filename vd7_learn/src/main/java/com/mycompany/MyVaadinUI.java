2d6226a25e7d2ad81a2d95b3d891cb39e5f99368
gongwenwei <gongwenwei@gmail.com>
2013-02-07 10:05:35 +0800

    vaadin 7

---
+++ vd7_learn/src/main/java/com/mycompany/MyVaadinUI.java
@@ -0,0 +1,32 @@
+package com.mycompany;
+
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.ui.Label;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+/**
+ * The Application's "main" class
+ */
+@SuppressWarnings("serial")
+public class MyVaadinUI extends UI
+{
+
+    @Override
+    protected void init(VaadinRequest request) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.setMargin(true);
+        setContent(layout);
+        
+        Button button = new Button("Click Me");
+        button.addClickListener(new Button.ClickListener() {
+            public void buttonClick(ClickEvent event) {
+                layout.addComponent(new Label("Thank you for clicking"));
+            }
+        });
+        layout.addComponent(button);
+    }
+
+}
